761088776a1666adea837399d2020c43e2736a72
Feride_queen <faridka0@gmail.com>
2022-12-09 17:51:35 +0300

    решила 10 задание 2 семинара

---
+++ Work.10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int n1 = number / 100;
+int n2= number / 10;
+int n3 = number % 10;
+Console.WriteLine (n2 % 10);
